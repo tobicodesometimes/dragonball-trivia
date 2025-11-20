50827dd9d4ab96ffd65928b64bf6cf44f5a86d19
tobicodesometimes <onasanyatobii@gmail.com>
2025-11-19 22:36:26 -0500

    basic styling for layout

--- frontend/dragonball-trivia/src/App.jsx
+++ frontend/dragonball-trivia/src/App.jsx
@@ -1,14 +1,1 @@
 
-import './App.css'
-
-function App() {
-  
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default App
--- frontend/dragonball-trivia/src/components/Quiz.jsx
+++ frontend/dragonball-trivia/src/components/Quiz.jsx
@@ -1,25 +1,25 @@
 // src/components/Quiz.jsx
-function Quiz ({question, currentIndex, totalQuestions, score, onAnswer}) {
-    if (!question) return null;  
+function Quiz({ question, currentIndex, totalQuestions, score, onAnswer }) {
+  if (!question) return null;
 
-    return (
-        <div className="quiz">
-            <p>
-                Question {currentIndex + 1} / {totalQuestions}
-            </p>
-            <h2>{question.questionText}</h2>
+  return (
+    <div className="quiz">
+      <p>
+        Question {currentIndex + 1} / {totalQuestions}
+      </p>
+      <h2>{question.questionText}</h2>
 
-            <div className="options">
-                {question.option.map((opt, index) => (
-                    <button key = {index} onClick = {() => onAnswer(index)}>
-                        {opt}
-                    </button>
-                ))}
-            </div>
+      <div className="options">
+        {question.options.map((opt, index) => (
+          <button key={index} onClick={() => onAnswer(index)}>
+            {opt}
+          </button>
+        ))}
+      </div>
 
-            <p>Score: {score}</p>
-        </div>
-    );
+      <p>Score: {score}</p>
+    </div>
+  );
 }
 
 export default Quiz;

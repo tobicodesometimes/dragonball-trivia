d9f88dd19abdd5420fca45776f0b3a523f7e15a5
tobicodesometimes <onasanyatobii@gmail.com>
2025-11-20 10:22:50 -0500

    setup for App.jsx

--- frontend/dragonball-trivia/src/App.jsx
+++ frontend/dragonball-trivia/src/App.jsx
@@ -1,1 +1,11 @@
+// src/App.jsx 
+import { useState } from "react";
+import StartScreen from "./components/StartScreen";
+import Quiz from "./components/Quiz";
+import ResultScreen from "./components/ResultScreen";
+import Leaderboard from "./components/Leaderboard";
+import './App.css';
+
+const API_BASE_URL = 'http://localhost:5000';
+
 

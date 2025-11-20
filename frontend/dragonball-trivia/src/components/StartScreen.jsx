94f86590b60c0c3d53efbf216942dca7bbdeaa41
tobicodesometimes <onasanyatobii@gmail.com>
2025-11-19 20:57:31 -0500

    component for StartScreen page

---
+++ frontend/dragonball-trivia/src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+function StartScreen ({onStart, loading, error}) {
+    return (
+        <div className = "start-screen">
+            <p>Test your Dragon Ball knowledge!</p>
+            {error && <p className = "error"> {error} </p>}
+            <button onClick = {onStart} disabled={loading}>
+            {loading ? 'Loading questions...' : 'Start Quiz'}
+            </button>
+        </div>
+    );
+}
+
+export default StartScreen; 
